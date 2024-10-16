193b2e00ed7d0d4847446be5c6d965de21ac3396
netesf13d <brice.ravon@proton.me>
2024-10-16 10:31:02 +0200

    Update project 2

---
+++ 2_data_collection_and_management/sql_stuff.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+import psycopg2
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base, Session
+
+
+Base = declarative_base()
+
+
+"""
+Creation of PostgreSQL database container and start server
+$ initdb -D <name>
+$ pg_ctl -D <name> -l logfile start
+
+Create user and database
+$ createuser <username>
+$ createdb --owner=<username> <dbname>
+
+Connect with
+"postgresql://<usenamer>@<hostname>:<port>/<dbname>"
+
+Stop server
+$ ps aux | grep postgres
+$ kill </postgres -D <name> PID>
+"""
+
+
+
+# =============================================================================
+# 
+# =============================================================================
+
+class Customer(Base):
+    __tablename__ = "customers"
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    country = Column(String)
+    job = Column(String)
+    age = Column(Integer)
+    
+    
+    def __repr__(self):
+        str_ = (
+            "<"
+            f"User(id={self.id}, "
+            f"name={self.name}, "
+            f"country={self.country}, "
+            f"job={self.job}, "
+            f"age={self.age}"
+            ">"
+        )
+        return str_
+
+
+# =============================================================================
+# 
+# =============================================================================
+
+host = "127.0.0.1"
+port = 5432
+user = "user"
+passwd = ""
+dbname = "userdb"
+
+sqlalchemy_driver = "+psycopg2"
+conn_str = f"postgresql{sqlalchemy_driver}://{user}@{host}:{port}/{dbname}"
+
+
+engine = create_engine(conn_str, echo=True)
+
+Base.metadata.create_all(engine)
+
+
+# =============================================================================
+# 
+# =============================================================================
+
+customers = [
+    Customer(id=1, name="Sauerkraut", country="Germany", job="engineer", age=37),
+    Customer(id=2, name="Jones", country="United Kingdom", job="journalist", age=52),
+    Customer(id=3, name="Dupont", country="France", job="dancer", age=25),
+]
+
+with Session(engine) as session:
+    for customer in customers:
+        session.add(customer)
+    session.commit()
+    
+    customer = session.query(Customer)
+    print(customer.all())
+
+
+
+
+# conn = psycopg2.connect(conn_str)
+# conn = psycopg2.connect(dbname="userdb", host="127.0.0.1", port=5432, user="user")
+
+
+
+
+
--- 2_data_collection_and_management/utils/web_scraping.py
+++ 2_data_collection_and_management/utils/web_scraping.py
@@ -3,91 +3,53 @@
 """
 This module implements web scraping utilities for the project. The functions
 implemented are:
-- `get_coords`: get geographic coordinates for a selected destination
+- `get_coords`: get geographic coordinates for a selected location
 - ``: get weather data at given geographic coordinates
-- ``: get information about hotels in the destination surroundings
+- ``: get information about hotels in the location surroundings
 """
 
 import asyncio
+import warnings
 
 import requests
+import scrapy
+
 
 
 # =============================================================================
 # 
 # =============================================================================
 
+coords = (-0.7024738, 49.2764624)
+coords = {'lon': -0.7024738, 'lat': 49.2764624}
+display_name = ""
 
-def get_coords(destination: dict[str, str], **kwargs)-> tuple[float, float]:
-    """
-    Get the geographic coordinates of a selected destination using
-    [Nominatim API](https://nominatim.org/).
+params = {
+    'ss': display_name,
+    'checkin': '2024-10-24',
+    'checkout': '2024-11-01',
+    'group_adults': 2,
+    'group_children': 1,
+    'no_rooms': 1}
+
+
+
+class BookingSpider(scrapy.Spider):
+    name = "hotels_info"
     
-
-    Parameters
-    ----------
-    destination : dict[str, str]
-        DESCRIPTION.
-    kwargs : str
-        Additional arguments passed to Nominatim API.
-        
-
-    Returns
-    -------
-    tuple[float, float]
-        The (latitude, longitude) of the destination.
-    
-    Examples
-    --------
-    >>> get_coords({'city': "Bayeux", 'country': "France"})
-    ()
-
-    """
-    params = kwargs | destination | {'format': 'geojson'}
-    r = requests.get("https://nominatim.openstreetmap.org/search",
-                     params=params)
-    match r.status_code:
-        case 200:
-            return filter_results(r.json()['features'])
-        case _:
-            raise ValueError("request failed")
-    
-
-
-def filter_results(results: list[dict])-> tuple[float, float]:
-    """
-    
-
-    Parameters
-    ----------
-    res : list[dict]
-        DESCRIPTION.
-
-    Returns
-    -------
-    tuple[float, float]
-        DESCRIPTION.
-
-    """
-    favorite_address_types = [
-        'city',
-        'town',
-    ]
-    for address_type in favorite_address_types:
-        for res in results:
-            if (res['type'] == 'administrative'
-                and res['addresstype'] == address_type):
-                return tuple(res['geometry']['coordinates'])
-    # default to the first result
-    return tuple(results[0]['geometry']['coordinates'])
+    def start_requests(self):
+        pass
         
     
+    def parse():
+        pass
 
-# r = requests.get("https://nominatim.openstreetmap.org/status",
-#                   params={'format': "json"})
 
-r = requests.get("https://nominatim.openstreetmap.org/search",
-                  params={'city': "Bayeux",
-                          'country': "France",
-                          'format': "geojson"})
+url = """
+https://www.booking.com/searchresults.en-gb.html?ss=94250%2C+France&ssne=Paris&ssne_untouched=Paris&label=gen173nr-1BCAEoggI46AdIM1gEaE2IAQGYAQm4ARjIAQ_YAQHoAQGIAgGoAgS4AoCAtLgGwAIB0gIkMzJiOGJhZTctNDk2Yi00ZTk5LTg2MWQtYzFjYmJkOTMyZjFk2AIF4AIB&sid=438db7b646a86ed2276c339865eec369&aid=304142&lang=en-gb&sb=1&src_elem=sb&src=searchresults&dest_id=9608074&dest_type=hotel&ac_position=0&ac_click_type=b&ac_langcode=en&ac_suggestion_list_length=1&search_selected=true&search_pageview_id=53ad50f3c34f08d7&ac_meta=GhA1M2FkNTBmM2MzNGYwOGQ3IAAoATICZW46DTk0MjUwLCBGcmFuY2VAAEoAUAA%3D&checkin=2024-10-18&checkout=2024-10-25&group_adults=2&no_rooms=1&group_children=0#map_opened
+"""
+
+url2 = """
+https://www.booking.com/searchresults.en-gb.html?src=searchresults&dest_type=landmark&latitude=48.2494853&longitude=7.3444831&ac_position=0&ac_click_type=g&ac_langcode=en-gb&ac_suggestion_list_length=100&search_selected=true&checkin=2024-10-18&checkout=2024-10-25&group_adults=2&no_rooms=1&group_children=0#map_opened
+"""
 
