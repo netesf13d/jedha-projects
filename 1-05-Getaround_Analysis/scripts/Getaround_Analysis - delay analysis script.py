79b2276dfb1d9c1524dbaa5fb6e21c9320c3327d
netesf13d <brice.ravon@proton.me>
2025-03-13 14:40:00 +0100

    work on 1-05 Getaround

--- 1-05-Getaround_Analysis/scripts/Getaround_Analysis - delay analysis script.py
+++ 1-05-Getaround_Analysis/scripts/Getaround_Analysis - delay analysis script.py
@@ -67,98 +67,98 @@ df_ / df_.T.groupby('checkin_type').sum()
 Higher probability of cancellation when connect
 """
 
+
+
+
 # %%
+"""
+### Checkout delay
 
-# cancelled_checkout_delay = 0
-# ended_checkout_delay = 0
 
-# checkout_delay = {}
-# for state, df_ in df.groupby('state'):
-#     checkout_delay[state] = df_.loc[:, ['checkin_type', 'delay_at_checkout_in_minutes']]
+"""
 
-checkout_delay = {}
+##
+df_ = df.groupby(['checkin_type', 'state']).agg(lambda x: x.isnull().sum())
+df_
+
+"""
+Almost all null `delay_at_checkout_in_minutes` when canceled
+"""
+
+
+##
+delay_vals = np.logspace([0], [5], 21)
 checkout_distrib = {}
-for item, df_ in df.groupby(['checkin_type', 'state']):
+for (checkin,), df_ in df.groupby(['checkin_type']): #, 'state']):
     data = df_['delay_at_checkout_in_minutes'].to_numpy()
     data = data[~np.isnan(data)]
-    # checkout_delay[item] = data[~np.isnan(data)]
-    print(data)
-    n = len(data)
-    checkout_delay[item] = [
-        np.sum(data > np.logspace([0], [5], 21), axis=1) / n,
-        np.sum(data < -np.logspace([0], [5], 21), axis=1) / n,
+    checkout_distrib[checkin] = [
+        np.sum(data >= delay_vals, axis=1) / len(data),
+        np.sum(data <= -delay_vals, axis=1) / len(data),
         ]
-    
-    
-    # if state == 'ended':
-    #     checkout_delay[checkin][0] = df_['time_delta_with_previous_rental_in_minutes'].to_numpy()
-    # if state == 'canceled':
-    #     rental_delay[checkin][1] = df_['time_delta_with_previous_rental_in_minutes'].to_numpy()
 
 
-# checkout_distrib = {}
-
-
-
-# %%
-
-# cols = ['checkin_type', 'state', 'delay_at_checkout_in_minutes',
-#        'time_delta_with_previous_rental_in_minutes']
-
-# data = df.groupby(['checkin_type', 'state']).count()['car_id'].to_numpy()
-
-
-
+##
 fig1, axs1 = plt.subplots(
     nrows=1, ncols=2, figsize=(9, 4), dpi=200,
-    gridspec_kw={'left': 0.06, 'right': 0.97, 'top': 0.85, 'bottom': 0.13,
+    gridspec_kw={'left': 0.075, 'right': 0.97, 'top': 0.85, 'bottom': 0.13,
                  'wspace': 0.18})
 fig1.suptitle('Figure 1: checkout delay distribution', x=0.02, ha='left')
 
+labels = [r'$P\,(\mathrm{delay} \geq \tau)$', r'$P\, (\mathrm{delay} \leq - \tau)$']
 
-handles = [Patch(facecolor='tab:blue', alpha=1, label='ended'),
-           Patch(facecolor='tab:orange', alpha=1, label='ended')]
-
-axs1[0].set_title("Checkout delay distribution")
-axs1[0].hist(df['delay_at_checkout_in_minutes'], bins=np.linspace(-100, 100, 22))
+axs1[0].set_title("Mobile checkin")
+line0, = axs1[0].plot(delay_vals, checkout_distrib['mobile'][0],
+                      linestyle='', marker='.', markersize=10, color='tab:blue')
+line1, = axs1[0].plot(delay_vals, checkout_distrib['mobile'][1],
+             linestyle='', marker='.', markersize=10, color='tab:orange')
+axs1[0].text(0.035, 0.06, r'$P\,(\mathrm{delay} \geq 0) = '
+             fr'{checkout_distrib["mobile"][0][0]:.3f}$',
+             transform=axs1[0].transAxes, fontsize=9,
+             bbox={'boxstyle': 'round,pad=0.5', 'facecolor': '0.92'})
 axs1[0].grid(visible=True, linewidth=0.3)
-# axs1[0].set_xlim(-15, 735)
-axs1[0].set_xlabel("Checkout delay (min)")
-axs1[0].set_ylabel('Counts')
+axs1[0].set_xscale('log')
+axs1[0].set_xlim(1, 1e5)
+axs1[0].set_yscale('log')
+axs1[0].set_ylim(1e-4, 1)
+axs1[0].set_xlabel(r"Checkout delay $\tau$ (min)")
+axs1[0].set_ylabel('Inverse cumm. prob.')
+axs1[0].legend(handles=[line0, line1], labels=labels)
 
 
-
-axs1[1].set_title("Delay with previous rental")
-axs1[1].hist(df['time_delta_with_previous_rental_in_minutes'],
-             bins=np.linspace(-15, 735, 26))
+axs1[1].set_title("Getaround connect checkin")
+line0, = axs1[1].plot(delay_vals, checkout_distrib['connect'][0],
+                      linestyle='', marker='.', markersize=10, color='tab:blue')
+line1, = axs1[1].plot(delay_vals, checkout_distrib['connect'][1],
+             linestyle='', marker='.', markersize=10, color='tab:orange')
+axs1[1].text(0.035, 0.06, r'$P\,(\mathrm{delay} \geq 0) = '
+             fr'{checkout_distrib["connect"][0][0]:.3f}$',
+             transform=axs1[1].transAxes, fontsize=9,
+             bbox={'boxstyle': 'round,pad=0.5', 'facecolor': '0.92'})
 axs1[1].grid(visible=True, linewidth=0.3)
-axs1[1].set_xlim(-15, 735)
-axs1[1].set_xticks(np.linspace(0, 720, 7))
-axs1[1].set_xticks(np.linspace(60, 640, 6), minor=True)
-axs1[1].set_xlabel("Delay with previous rental")
+axs1[1].set_xscale('log')
+axs1[1].set_xlim(1, 1e5)
+axs1[1].set_yscale('log')
+axs1[1].set_ylim(1e-4, 1)
+axs1[1].set_xlabel(r"Checkout delay $\tau$ (min)")
+axs1[1].legend(handles=[line0, line1], labels=labels)
 
-
-# cmap1 = plt.get_cmap('plasma').copy()
-# cmap1.set_extremes(under='0.7', over='0.5')
-# axs1[2].set_aspect('equal')
-
-# heatmap = axs1[2].pcolormesh(data.reshape((2, 2))/np.sum(data), linewidth=0.6,
-#                              edgecolors='0.3', cmap=cmap1, vmin=0, vmax=1)
-# axs1[2].set_xticks([0.5, 1.5], ['cancelled', 'ended'])
-# axs1[2].set_yticks([0.5, 1.5], ['connect', 'mobile'])
-# for (i, j), val in np.ndenumerate(data.reshape((2, 2))):
-#     axs1[2].text(0.5+i, 0.5+j, val, ha='center', va='center')
-
-# div = make_axes_locatable(axs1[2])
-# axs1_cb = div.append_axes('top', size="7%", pad="7%")
-# fig1.add_axes(axs1_cb)
-# fig1.colorbar(heatmap, cax=axs1_cb, orientation="horizontal", ticklocation='top')
 
 plt.show()
 
+"""
+Figure 1 presents the distribution of checkout delays.
+"""
+
 
 # %%
+"""
+### Delay with previous rental
 
+
+"""
+
+##
 rental_delay = {'mobile': [0, 0], 'connect': [0, 0]}
 for (checkin, state), df_ in df.groupby(['checkin_type', 'state']):
     if state == 'ended':
@@ -166,38 +166,71 @@ for (checkin, state), df_ in df.groupby(['checkin_type', 'state']):
     if state == 'canceled':
         rental_delay[checkin][1] = df_['time_delta_with_previous_rental_in_minutes'].to_numpy()
 
+##
+delay_bins = np.linspace(-30, 750, 14)
+delay_vals = (delay_bins[1:] + delay_bins[:-1]) / 2
 
+## 'mobile' delay histograms : ended, canceled, cancelation prob 
+delay_hist_me, _ = np.histogram(rental_delay['mobile'][0], bins=delay_bins)
+delay_hist_mc, _ = np.histogram(rental_delay['mobile'][1], bins=delay_bins)
+delay_hist_mfrac = delay_hist_mc / (delay_hist_mc + delay_hist_me)
+
+## 'connect' delay histograms : ended, canceled, cancelation prob 
+delay_hist_ce, _ = np.histogram(rental_delay['connect'][0], bins=delay_bins)
+delay_hist_cc, _ = np.histogram(rental_delay['connect'][1], bins=delay_bins)
+delay_hist_cfrac = delay_hist_cc / (delay_hist_cc + delay_hist_ce)
+
+##
 fig2, axs2 = plt.subplots(
-    nrows=1, ncols=2, figsize=(8, 4), dpi=200,
-    gridspec_kw={'left': 0.06, 'right': 0.97, 'top': 0.85, 'bottom': 0.13,
-                 'wspace': 0.18})
+    nrows=1, ncols=2, figsize=(9, 4), dpi=200,
+    gridspec_kw={'left': 0.07, 'right': 0.94, 'top': 0.85, 'bottom': 0.13,
+                 'wspace': 0.24})
+axs2_twin = [ax.twinx() for ax in axs2]
 fig2.suptitle('Figure 2: Distribution of delays with previous rental', x=0.02, ha='left')
+
+
 
 handles = [Patch(facecolor='tab:blue', alpha=1, label='ended'),
            Patch(facecolor='tab:orange', alpha=1, label='canceled')]
+labels = ['ended', 'canceled', 'cancelation prob.']
 
 axs2[0].set_title("Mobile checkin")
-axs2[0].hist(rental_delay['mobile'], bins=np.linspace(-15, 735, 26),
+axs2[0].hist(rental_delay['mobile'], bins=np.linspace(-30, 750, 14),
              stacked=False, density=False)
+
+line, = axs2_twin[0].plot(delay_vals, delay_hist_mfrac, color='tab:green',
+                  marker='o', markersize=5)
+axs2_twin[0].set_ylim(0, 0.425)
+axs2_twin[0].set_yticks([0, 0.1, 0.2, 0.3, 0.4])
+axs2_twin[0].set_yticks([0.05, 0.15, 0.25, 0.35], minor=True)
+
 axs2[0].grid(visible=True, linewidth=0.3)
-axs2[0].set_xlim(-15, 735)
+axs2[0].set_xlim(-30, 750)
 axs2[0].set_xticks(np.linspace(0, 720, 7))
-axs2[0].set_xticks(np.linspace(60, 640, 6), minor=True)
-axs2[0].set_ylim(0, 140)
+axs2[0].set_xticks(np.linspace(60, 660, 6), minor=True)
 axs2[0].set_xlabel("Delay with previous rental (min)")
-axs2[0].legend(handles=handles)
+axs2[0].set_ylim(0, 170)
+axs2[0].set_ylabel("Counts")
+axs2[0].legend(handles=handles + [line], labels=labels)
 
 
 axs2[1].set_title("Getaround connect checkin")
-axs2[1].hist(rental_delay['connect'], bins=np.linspace(-15, 735, 26),
+axs2[1].hist(rental_delay['connect'], bins=np.linspace(-30, 750, 14), # np.linspace(-15, 735, 26)
              stacked=False, density=False)
+
+line, = axs2_twin[1].plot(delay_vals, delay_hist_cfrac, color='tab:green',
+                          marker='o', markersize=5)
+axs2_twin[1].set_ylim(0, 0.425)
+axs2_twin[1].set_yticks([0, 0.1, 0.2, 0.3, 0.4])
+axs2_twin[1].set_yticks([0.05, 0.15, 0.25, 0.35], minor=True)
+
 axs2[1].grid(visible=True, linewidth=0.3)
-axs2[1].set_xlim(-15, 735)
+axs2[1].set_xlim(-30, 750)
 axs2[1].set_xticks(np.linspace(0, 720, 7))
-axs2[1].set_xticks(np.linspace(60, 640, 6), minor=True)
-axs2[1].set_ylim(0, 140)
+axs2[1].set_xticks(np.linspace(60, 660, 6), minor=True)
+axs2[1].set_ylim(0, 170)
 axs2[1].set_xlabel("Delay with previous rental (min)")
-axs2[1].legend(handles=handles)
+axs2[1].legend(handles=handles + [line], labels=labels)
 
 
 plt.show()
@@ -209,8 +242,111 @@ Figure 2
 # %%
 
 """
+### Cancel probability
+
 
 """
+
+##
+prev_rental_cols = ['rental_id', 'delay_at_checkout_in_minutes']
+curr_rental_cols = ['previous_ended_rental_id', 'checkin_type', 'state',
+                    'time_delta_with_previous_rental_in_minutes']
+df_prev = df.loc[:, prev_rental_cols]
+df_curr = df.loc[:, curr_rental_cols]
+df2 = pd.merge(df_prev, df_curr, how='inner', left_on='rental_id',
+               right_on='previous_ended_rental_id')
+df2 = df2.assign(is_canceled=(df2['state'] == 'canceled'))
+
+df2.describe(include='all')
+# df_ = df_.loc[:, ['delay_at_checkout_in_minutes', 'checkin_type', 'state', 'time_delta_with_previous_rental_in_minutes']]
+
+##
+# delay_vals = np.logspace([0], [4], 21)
+# checkout_distrib2 = {}
+# for item, df_ in df2.groupby(['checkin_type', 'state']):
+#     data = df_['delay_at_checkout_in_minutes'].to_numpy()
+#     data = data[~np.isnan(data)]
+#     checkout_distrib2[item] = [
+#         np.sum(data >= delay_vals, axis=1) / len(data),
+#         np.sum(data <= -delay_vals, axis=1) / len(data),
+#         ]
+
+##
+# df_ = pd.concat([df2['state'] == 'canceled', df2.loc[:, ['checkin_type']]], axis=1)
+# df2_ = df.loc[:, ['delay_at_checkout_in_minutes', 'checkin_type']]
+# df2_.assign(is_canceled=(df2['state'] == 'canceled'))
+
+delay_vals = np.concatenate([-np.logspace(4, 0, 21), np.logspace(0, 4, 21)])
+cancel_prob = {}
+for (checkin,), df_ in df2.groupby(['checkin_type']):
+    data = np.zeros_like(delay_vals)
+    for i, val in enumerate(delay_vals):
+        loc = (df_['delay_at_checkout_in_minutes'] >= val)
+        # print(df_.loc[loc, 'is_canceled'].mean())
+        data[i] = df_.loc[loc, 'is_canceled'].mean()
+    cancel_prob[checkin] = data
+
+
+
+#%%
+
+##
+fig3, axs3 = plt.subplots(
+    nrows=1, ncols=2, figsize=(9, 4), dpi=200,
+    gridspec_kw={'left': 0.075, 'right': 0.97, 'top': 0.85, 'bottom': 0.13,
+                 'wspace': 0.18})
+fig3.suptitle('Figure 1: checkout delay distribution', x=0.02, ha='left')
+
+labels = [r'$P\,(\mathrm{delay} \geq \tau)$', r'$P\, (\mathrm{delay} \leq - \tau)$',
+          'a', 'b']
+
+axs3[0].set_title("Mobile checkin")
+line0, = axs3[0].plot(delay_vals, cancel_prob['mobile'],
+                      linestyle='-', marker='o', markersize=6, color='tab:blue')
+line1, = axs3[0].plot(delay_vals, cancel_prob['mobile'],
+             linestyle='-', marker='o', markersize=6, color='tab:orange')
+# line2, = axs3[0].plot(delay_vals, checkout_distrib2[('mobile', 'ended')][0],
+#                       linestyle='-', marker='v', markersize=6, color='tab:blue')
+# line3, = axs3[0].plot(delay_vals, checkout_distrib2[('mobile', 'ended')][1],
+#              linestyle='-', marker='v', markersize=6, color='tab:orange')
+# axs3[0].text(0.035, 0.06, r'$P\,(\mathrm{delay} \geq 0) = '
+#              fr'{checkout_distrib["mobile"][0][0]:.3f}$',
+#              transform=axs3[0].transAxes, fontsize=9,
+#              bbox={'boxstyle': 'round,pad=0.5', 'facecolor': '0.92'})
+axs3[0].grid(visible=True, linewidth=0.3)
+# axs3[0].set_xscale('log')
+axs3[0].set_xlim(-1e4, 1e4)
+# axs3[0].set_yscale('log')
+axs3[0].set_ylim(0, 1)
+axs3[0].set_xlabel(r"Checkout delay $\tau$ (min)")
+axs3[0].set_ylabel('Inverse cumm. prob.')
+# axs3[0].legend(handles=[line0, line1, line2, line3], labels=labels)
+
+
+axs3[1].set_title("Getaround connect checkin")
+line0, = axs3[1].plot(delay_vals, cancel_prob['connect'],
+                      linestyle='-', marker='o', markersize=6, color='tab:blue')
+line1, = axs3[1].plot(delay_vals, cancel_prob['connect'],
+                      linestyle='-', marker='o', markersize=6, color='tab:orange')
+# line2, = axs3[1].plot(delay_vals, checkout_distrib2[('connect', 'ended')][0],
+#                       linestyle='-', marker='v', markersize=6, color='tab:blue')
+# line3, = axs3[1].plot(delay_vals, checkout_distrib2[('connect', 'ended')][1],
+#                       linestyle='-', marker='v', markersize=6, color='tab:orange')
+# axs3[1].text(0.035, 0.06, r'$P\,(\mathrm{delay} \geq 0) = '
+#              fr'{checkout_distrib["connect"][0][0]:.3f}$',
+#              transform=axs3[1].transAxes, fontsize=9,
+#              bbox={'boxstyle': 'round,pad=0.5', 'facecolor': '0.92'})
+axs3[1].grid(visible=True, linewidth=0.3)
+axs3[1].set_xscale('log')
+axs3[1].set_xlim(1, 1e4)
+# axs3[1].set_yscale('log')
+axs3[1].set_ylim(0, 1)
+axs3[1].set_xlabel(r"Checkout delay $\tau$ (min)")
+# axs3[1].legend(handles=[line0, line1, line2, line3], labels=labels)
+
+
+plt.show()
+
 
 
 
